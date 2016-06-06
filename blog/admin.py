fc08c7dccdbe34080beba1f5c2981c2854412572
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-06-06 19:54:51 +0900

    admin 페이지에 blog.post 노출 / index 페이지에 post_list 노출

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
+from .models import Post
 from django.contrib import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content_size', 'created_at']
+
+    def content_size(self, post):
+        return '%s자' % intcomma(len(post.content))
+
+admin.site.register(Post, PostAdmin)
