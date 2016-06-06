b05b1755abb26e62f46fbf4144c8cc0750bc9478
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-06-06 21:15:08 +0900

    logout뷰 구현 / 템플릿단에 signup,login,logout 링크 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', login),
-    url(r'^signup/$', views.signup),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
